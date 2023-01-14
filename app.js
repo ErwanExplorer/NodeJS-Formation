4996ff1a209a0a5b84aa39d07518499c64d5a4ab
XloxTV <pro.xloxtv@gmail.com>
2023-01-14 13:21:36 +0100

    First Server Web

--- app.js
+++ app.js
@@ -1,5 +1,15 @@
-import { createReadStream, createWriteStream } from 'node:fs'
+import { createReadStream } from 'node:fs'
+import { createServer } from 'node:http'
+import { text } from 'stream/consumers'
 
-const stream = createReadStream('video.mp4')
-const writeStreeam = createWriteStream('video-copy.mp4')
-stream.pipe(writeStreeam)
+const ports = 8000
+const host = `https://localhost:${ports}`
+
+console.log(`Server is running on ${host}`)
+const server = createServer(async (req, res) => {
+    const file = createReadStream('index.html')
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    file.pipe(res)
+    //res.write(`Bonjour ${url.searchParams.get('name')}`)
+})
+server.listen(ports)
