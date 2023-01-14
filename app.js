2da5e532b0783e1f4929f0bf0bed6b215bf47347
XloxTV <pro.xloxtv@gmail.com>
2023-01-14 11:37:40 +0100

    Stream ex1

---
+++ app.js
@@ -0,0 +1,13 @@
+import { createReadStream } from 'node:fs'
+import { stat } from 'node:fs/promises'
+
+const stream = createReadStream('video.mp4')
+const { size } = await stat('video.mp4')
+let read = 0
+stream.on('data', (chunk) => {
+    read += chunk.length
+    console.log(Math.round(100 * read / size))
+})
+stream.on('close', () => {
+    console.log('close')
+})
