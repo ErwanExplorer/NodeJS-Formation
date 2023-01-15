853571852af2dba8ff159757f443e1f7190898d2
XloxTV <pro.xloxtv@gmail.com>
2023-01-15 14:45:07 +0100

    Utilisation npm

--- app.js
+++ app.js
@@ -1,43 +1,22 @@
-import { createReadStream } from 'node:fs'
-import { createServer } from 'node:http'
-import { create, index, remove, update } from './functions/api/todos.js'
-import { NotFoundError } from './functions/errors.js'
+import Fastify from 'fastify'
+import { findTodos, removeTodo } from './functions/todos_storage.js'
+const fastify = Fastify({
+    logger: false
+})
 
-createServer(async (req, res) => {
-    try {
-        res.setHeader('Content-Type', 'application/json')
-        const url = new URL(req.url, `http://${req.headers.host}`)
-        const endpoint = `${req.method} ${url.pathname}`
-        let results
-        switch (endpoint) {
-            case 'GET /':
-                res.setHeader('Content-Type', 'text/html')
-                createReadStream('index.html').pipe(res)
-                return
-            case 'GET /todos':
-                results = await index(req, res)
-                break
-            case 'POST /todos':
-                results = await create(req, res)
-                break
-            case 'DELETE /todos':
-                await remove(req, res, url)
-                break
-            case 'PUT /todos':
-                results = await update(req, res, url)
-                break
-            default:
-                res.writeHead(404)
-        }
-        if (results) {
-            res.write(JSON.stringify(results))
-        }
-    } catch (e) {
-        if (e instanceof NotFoundError) {
-            res.writeHead(404)
-        } else {
-            throw e
-        }
-    }
-    res.end()
-}).listen(3000)
+// Declare a route
+fastify.get('/todos', async (request, reply) => {
+    reply.send(await findTodos())
+})
+
+fastify.delete('/todos', async (request, reply) => {
+    await removeTodo(parseInt(request.query.id, 10))
+    reply.code(204)
+    console.log(request.query.id)
+})
+
+// Run the server!
+fastify.listen({ port: 3000 }, (err, address) => {
+    if (err) throw err
+    // Server is now listening on ${address}
+})
